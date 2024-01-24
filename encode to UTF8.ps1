db50b3df8f38ff3d7f98a9e3a8084438fd0cdfe2
Alcyone <40251145+alcyone-code@users.noreply.github.com>
2024-01-24 10:25:21 +0900

    Update encode to UTF8.ps1

--- encode to UTF8.ps1
+++ encode to UTF8.ps1
@@ -1,7 +1,7 @@
-write 'Convert To UTF8 : '
+write-host 'Convert To UTF8 : '
 foreach($filename in ls -name .\*.smi){
  (Get-Content -LiteralPath .\$filename) | Set-Content -Encoding UTF8 -LiteralPath .\$filename
- write $filename
+ write-host $filename
 }
 
 Read-Host "Enter to Exit"
